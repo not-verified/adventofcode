20bb1df7d349f64a7f2c7132de7c4fb93de60b33
not-verified <n-verified@protonmail.com>
2020-12-01 22:43:06 +0100

    Adds day 1 solutions.

---
+++ 2020/01/1.py
@@ -0,0 +1,17 @@
+import itertools
+
+threshold = int(2020/2)
+large_numbers = []
+small_numbers = []
+
+with open('input.txt', 'r') as f:
+    for line in f:
+        num = int(line)
+        if num >= threshold:
+            large_numbers.append(num)
+        if num <= threshold:
+            small_numbers.append(num)
+            
+for combo in itertools.product(small_numbers, large_numbers):
+    if (combo[0] + combo[1] == 2020):
+        print(combo[0] * combo[1])
